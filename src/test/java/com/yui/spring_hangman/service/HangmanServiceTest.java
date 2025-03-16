5d5ba5096bb75809e3db3193c387fc7e6e0b2e60
Paul Laborderie <plaborderie@pm.me>
2025-03-16 03:50:05 +0100

    More tests

--- src/test/java/com/yui/spring_hangman/service/HangmanServiceTest.java
+++ src/test/java/com/yui/spring_hangman/service/HangmanServiceTest.java
@@ -43,4 +43,42 @@ public class HangmanServiceTest {
         assertThat(guesses).hasSize(1);
         assertThat(guesses.getFirst()).isEqualTo("p");
     }
+
+    @Test
+    public void livesLeft_shouldReturnCorrectNumberOfLives() {
+        hangmanService.word = "test";
+        hangmanService.guesses.add("t");
+        hangmanService.guesses.add("p");
+
+        int livesLeft = hangmanService.livesLeft();
+
+        assertThat(livesLeft).isEqualTo(5);
+    }
+
+    @Test
+    public void isGameWon_shouldReturnTrueWhenAllLettersGuessed() {
+        hangmanService.word = "test";
+        hangmanService.guesses.add("t");
+        hangmanService.guesses.add("e");
+        hangmanService.guesses.add("s");
+
+        boolean gameWon = hangmanService.isGameWon();
+
+        assertThat(gameWon).isTrue();
+    }
+
+    @Test
+    public void isGameLost_shouldReturnTrueWhenNoLivesLeft() {
+        hangmanService.word = "test";
+        hangmanService.guesses.add("p");
+        hangmanService.guesses.add("q");
+        hangmanService.guesses.add("r");
+        hangmanService.guesses.add("u");
+        hangmanService.guesses.add("v");
+        hangmanService.guesses.add("w");
+
+        boolean gameLost = hangmanService.isGameLost();
+
+        assertThat(gameLost).isTrue();
+    }
 }
